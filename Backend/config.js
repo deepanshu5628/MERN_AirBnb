d336bf87a560d197f9e8e15b2bcd176bc5cfba71
deepanshu5628 <d2810201@gmail.com>
2024-05-03 11:48:16 +0530

    solved basic config error

--- Backend/config.js
+++ Backend/config.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 let connectdb = async () => {
-    // await mongoose.connect(process.env.DB_LINK_HOSTED)
-    await mongoose.connect(process.env.DB_LINK_LOCAL)
+    await mongoose.connect(process.env.DB_LINK_HOSTED)
+    // await mongoose.connect(process.env.DB_LINK_LOCAL)
         .then(() => console.log("connected to db successfullly"))
         .catch((error) => console.log("error in connecting to db", error));
 }
